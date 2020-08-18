82f13bfd280cd99b74a3a20feda11a8ab64a50b7
Lukasz Sliwinski <40170823+ls2716@users.noreply.github.com>
2020-08-18 14:16:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ with open('LICENSE') as f:
 
 setup(
     name='wikidictparser',
-    version='0.1.0',
+    version='0.1',
     # description='Sample package for Python-Guide.org',
     long_description=readme,
     author='Lukasz Sliwinski',
     author_email='luki3141@gmail.com',
-    url='https://github.com/ls2716/wikidictparser_package',
+    url='https://github.com/ls2716/wikidictparser_package/archive/v_01.tar.gz',
     license=license,
     install_requires=[
           'requests',
@@ -26,6 +26,16 @@ setup(
           'pandas',
           'lxml',
       ],
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ]
 )
 
